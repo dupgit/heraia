bb0cf11fca243d171e92d32aea2281a450cf6b36
toady <toady@localhost>
2008-08-27 07:57:53 +0000

    Pushing back libheraia skeleton

--- libheraia/include/libheraia.h
+++ libheraia/include/libheraia.h
@@ -2,21 +2,21 @@
 /*
   libheraia.h
   Heraia's library header
- 
+
   (C) Copyright 2008 Sébastien Tricaud, Olivier Delhomme
   e-mail : heraia@delhomme.org
   URL    : http://heraia.tuxfamily.org
- 
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2, or  (at your option) 
+  the Free Software Foundation; either version 2, or  (at your option)
   any later version.
- 
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY;  without even the implied warranty of
   MERCHANTABILITY  or  FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
- 
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
@@ -24,6 +24,6 @@
 #ifndef _LIBHERAIA_H_
 #define _LIBHERAIA_H_
 
-int foo(void);
+int library_test(void);
 
 #endif /* _LIBHERAIA_H_ */
--- libheraia/libheraia.c
+++ libheraia/libheraia.c
@@ -2,21 +2,21 @@
 /*
   libheraia.c
   Heraia's library
- 
+
   (C) Copyright 2008 Sébastien Tricaud, Olivier Delhomme
   e-mail : heraia@delhomme.org
   URL    : http://heraia.tuxfamily.org
- 
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2, or  (at your option) 
+  the Free Software Foundation; either version 2, or  (at your option)
   any later version.
- 
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY;  without even the implied warranty of
   MERCHANTABILITY  or  FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
- 
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
@@ -24,10 +24,9 @@
 #include <stdio.h>
 
 #include <libheraia.h>
-#include "../src/include/heraia_types.h"
 
-int foo(void)
+int library_test(void)
 {
-	printf("bar\n");
+	printf("Libheraia library test OK\n");
 	return 0;
 }
