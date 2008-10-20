c848f91edd5f6a54d3ec6b402be768d41e36440a
dup <dup@localhost>
2008-10-20 19:25:19 +0000

    0.0.5 release

--- src/include/heraia.h
+++ src/include/heraia.h
@@ -27,7 +27,7 @@
 #define HERAIA_AUTHORS "Olivier Delhomme"
 #define HERAIA_DATE "20 02 2005"
 #define HERAIA_LICENSE "GPL"
-#define HERAIA_VERSION "0.0.5 (00.00.2008)"
+#define HERAIA_VERSION "0.0.5 (20.10.2008)"
 
 typedef struct
 {
