9da7a7dd3e6bb747bcfeb6db4a4d5f3ce3a9a15d
dup <dup@localhost>
2009-01-25 18:34:49 +0000

    Changed the script to have -dev package separated from others and added Description and check_dependencies.sh files to Makefile.am (so that they get distributed with the package)

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -13,15 +13,27 @@ grep -v "^\(/tmp\|/dev\|/proc\)" | sort | uniq) >/tmp/files 2>/dev/null
 
 cat /tmp/files | cut -d":" -f1 | sed -e s/,\ /'\n'/g | sort | uniq >/tmp/packages
 
-echo "This package depends on :"
+rm -f /tmp/packages-build-dep /tmp/packages-dep
 
 for p in $(cat /tmp/packages); 
  do
    version=$(dpkg -s $p | grep ^Version | cut -f2 -d":");
-   echo -n "$p (>=$version), ";
+
+	if [[ $(echo $p | grep -e "-dev") ]]; then
+		echo -n "$p (>=$version), " >>/tmp/packages-build-dep;
+	else
+		echo -n "$p (>=$version), " >>/tmp/packages-dep;
+	fi;
  done;
 
-echo -e "\b\b ";
+echo -e "\b\b " >>/tmp/packages-build-dep
+echo -e "\b\b " >>/tmp/packages-dep
+
+echo "This package depends on :"
+
+cat /tmp/packages-build-dep
+cat /tmp/packages-dep
+
 
 # Cleaning a bit...
-rm -f /tmp/log /tmp/files /tmp/packages
+#rm -f /tmp/log /tmp/files /tmp/packages /tmp/packages-build-dep /tmp/packages-dep
