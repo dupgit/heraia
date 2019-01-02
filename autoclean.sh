0a6305288425ee0d0f96057eac8f240a8fcf68ec
dupgit <olivier.delhomme@free.fr>
2019-01-02 21:25:39 +0100

    Refactors autoclean.sh

--- autoclean.sh
+++ autoclean.sh
@@ -3,50 +3,39 @@
 # Author : Olivier Delhomme
 # Licence : GNU GPL v2
 #
+
+##
+# Deletes a file
+# first argument is a file's filename to be deleted
+#
+function delete_files {
+
+    for file in "${@}"; do
+        echo "${file}"
+        rm -f "${file}"
+    done
+}
+
+
+##
+# Deletes a file everywhere in the current path
+#
+function delete_files_everywhere {
+
+    for file in "${@}"; do
+        echo "find . -name ${file} -exec rm -f {} \;"
+        find . -name "${file}" -exec rm -f {} \;
+    done
+}
+
+
 if [ -f Makefile ];
  then
     make distclean
 fi;
-rm -f aclocal.m4
-echo "rm -f aclocal.m4"
-rm -fr autom4te.cache
-echo "rm -fr autom4te.cache"
-rm -f config.guess
-echo "rm -f config.guess"
-rm -f config.sub
-echo "rm -f config.sub"
-rm -f depcomp
-echo "rm -f depcomp"
-rm -f INSTALL
-echo "rm -f INSTALL"
-rm -f install-sh
-echo "rm -f install-sh"
-rm -f ltmain.sh
-echo "rm -f ltmain.sh"
-rm -f missing
-echo "rm -f missing"
-find . -name "Makefile" -exec rm -f {} \;
-echo "find . -name Makefile -exec rm -f {} \;"
-find . -name "Makefile.in" -exec rm -f {} \;
-echo "find . -name Makefile.in -exec rm -f {} \;"
-rm -f configure
-echo "rm -f configure"
-rm -f config.status config.log
-echo "rm -f config.status config.log"
-rm -f libtool
-echo "rm -f libtool"
-find . -name "*~" -exec rm -f {} \;
-echo "find . -name *~ -exec rm -f {} \;"
-find . -name "*#" -exec rm -f {} \;
-echo "find . -name *# -exec rm -f {} \;"
-find . -name *.gcd[ao] -exec rm -f {} \;
-echo "find . -name *.gcd[ao] -exec rm -f {} \;"
-find . -name *.gcov -exec rm -f {} \;
-echo "find . -name *.gcov -exec rm -f {} \;"
-find . -name *.gcno -exec rm -f {} \;
-echo "find . -name *.gcno -exec rm -f {} \;"
-find . -name gmon.out -exec rm -f {} \;
-echo "find . -name gmon.out -exec rm -f {} \;"
 
-
-
+delete_files "aclocal.m4" "autom4te.cache" "config.guess" "config.sub" "depcomp" 
+delete_files "INSTALL" "install-sh" "ltmain.sh" "missing" "configure"
+delete_files "config.status" "config.log" "libtool" 
+delete_files_everywhere "Makefile" "Makefile.in" '*~' '*#"' '*.gcd[ao]' '*.gcov'
+delete_files_everywhere '*.gcno' 'gmon.out'
