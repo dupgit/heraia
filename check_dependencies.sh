9bf8e541ea7a6b6d07107b4ba327fe42cd1d4fa8
dupgit <olivier.delhomme@free.fr>
2019-01-03 21:21:06 +0100

    Cleans a bit check_dependencies.sh script

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Checks packages dependencies. Based on a script by Tamir ?? 
 # (http://ubuntuforums.org/showthread.php?t=51003)
@@ -11,19 +11,19 @@ strace -f -o /tmp/log ./configure >/dev/null 2>&1
 dpkg -S $(grep open /tmp/log | perl -pe 's!.* open\(\"([^\"]*).*!$1!' | grep "^/" |\
 grep -v "^\(/tmp\|/dev\|/proc\)" | sort | uniq) >/tmp/files 2>/dev/null
 
-cat /tmp/files | cut -d":" -f1 | sed -e s/,\ /'\n'/g | sort | uniq >/tmp/packages
+cut -d":" -f1 /tmp/files | sed -e s/,\ /'\n'/g | sort | uniq >/tmp/packages
 
 rm -f /tmp/packages-build-dep /tmp/packages-dep
 
 for p in $(cat /tmp/packages); 
  do
-   dpkg -s $p >/tmp/packages-desc;
+   dpkg -s "${p}" >/tmp/packages-desc;
    version=$(grep ^Version /tmp/packages-desc | cut -f2 -d":");
 
 	if [[ $(grep ^Section /tmp/packages-desc | grep -e "devel") ]]; then
-		echo -n "$p (>=$version), " >>/tmp/packages-build-dep;
+		echo -n "${p} (>=${version}), " >>/tmp/packages-build-dep;
 	else
-		echo -n "$p (>=$version), " >>/tmp/packages-dep;
+		echo -n "${p} (>=${version}), " >>/tmp/packages-dep;
 	fi;
  done;
 
